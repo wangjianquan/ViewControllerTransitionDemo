99d1d61ddd03bddc3d2fa4d180601988d72bb5e0
landixing <landixing@landixings-MacBook-Pro.local>
2017-06-29 10:09:45 +0800

    init

---
+++ ViewControllerTransition/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  ViewControllerTransition
+//
+//  Created by landixing on 2017/6/27.
+//  Copyright © 2017年 WJQ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    fileprivate lazy var dissolveFromVC: DissolveFirstVC = DissolveFirstVC()
+    fileprivate lazy var bottomFirstVC: BottomFirstVC = BottomFirstVC()
+    fileprivate lazy var swipeFirstVC: SwipeFirstVC = SwipeFirstVC()
+    
+    fileprivate lazy var fromRightFirstVC: FromRightFirstVC = FromRightFirstVC()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func present_Bottom() {
+        navigationController?.pushViewController(bottomFirstVC, animated: true)
+    }
+
+    @IBAction func from_Right() {
+        
+        navigationController?.pushViewController(fromRightFirstVC, animated: true)
+    }
+    @IBAction func Dissolve() {
+        
+        
+        navigationController?.pushViewController(dissolveFromVC, animated: true)
+        
+    
+    }
+    @IBAction func Swipe() {
+        navigationController?.pushViewController(swipeFirstVC, animated: true)
+        
+    }
+    
+    
+    
+
+}
+
